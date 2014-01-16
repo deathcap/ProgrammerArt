c09334e72ca3abadc40ef564c7d345b7c647af8d
deathcap <deathcap@gmx.co.uk>
2014-01-15 17:47:01 -0800

    Attempt to manually bitblt, still missing pixels

--- scripts/stitch.js
+++ scripts/stitch.js
@@ -10,7 +10,7 @@ var tileRows = 3, tileColumns = 1;
 
 var stitched = new PNG({width: tileWidth * tileRows, height: tileHeight * tileColumns, filterType:4});
 
-var x = 0, y = 0;
+var tileX = 0, tileY  = 0;
 
 files.forEach(function(pathFS) {
   console.log('pathFS='+pathFS);
@@ -22,11 +22,24 @@ files.forEach(function(pathFS) {
   if (png.width !== tileWidth || png.height !== tileHeight)
     throw new Error('unexpected dimensions on '+pathFS+': '+png.width+'x'+png.height+' !== '+tileWidth+'x'+tileHeight);
 
-  png.bitblt(stitched, 0, 0, tileWidth, tileHeight, x * tileWidth, y * tileHeight);
+  //png.bitblt(stitched, 0, 0, tileWidth, tileHeight, tileX * tileWidth, tileY * tileHeight);
 
-  x += 1;
+  for (var x = 0; x < png.width; ++x) {
+    for (var y = 0; y < png.height; ++y) {
+      var idx = (png.width * y + x) << 2;
+
+      var idxS = idx;
+
+      for (var c = 0; c < 4; ++c) {
+        stitched.data[idxS + c] = png[idx + c];
+        console.log(' data '+x+','+y+','+c+' = '+png.data[idx+c]);
+      }
+    }
+  }
+
+
+  tileX  += 1;
   //if (x > ) y += 1;
 });
 
 stitched.pack().pipe(fs.createWriteStream('terrain.png'));
-
