6e67cbd8db5b5a402720db3537b61d873144661d
deathcap <deathcap@gmx.co.uk>
2014-01-15 17:50:55 -0800

    Use parse() callback. Stripey pattern

--- scripts/stitch.js
+++ scripts/stitch.js
@@ -6,39 +6,42 @@ var fs = require('fs');
 var files = ['../textures/blocks/stone.png', '../textures/blocks/cobblestone.png', '../textures/blocks/gravel.png'];
 
 var tileWidth = 16, tileHeight = 16;
-var tileRows = 3, tileColumns = 1;
+var tileRows = 4, tileColumns = 1;
 
-var stitched = new PNG({width: tileWidth * tileRows, height: tileHeight * tileColumns, filterType:4});
+var stitched = new PNG({width: tileWidth * tileRows, height: tileHeight * tileColumns});
 
 var tileX = 0, tileY  = 0;
 
 files.forEach(function(pathFS) {
   console.log('pathFS='+pathFS);
   var png = new PNG();
-  png.parse(fs.readFileSync(pathFS));
+  png.parse(fs.readFileSync(pathFS), function(err, data) {
 
-  console.log('PARSED '+pathFS+', at '+png.width+'x'+png.height);
+    console.log('PARSED '+pathFS+', at '+png.width+'x'+png.height);
 
-  if (png.width !== tileWidth || png.height !== tileHeight)
-    throw new Error('unexpected dimensions on '+pathFS+': '+png.width+'x'+png.height+' !== '+tileWidth+'x'+tileHeight);
+    if (png.width !== tileWidth || png.height !== tileHeight)
+      throw new Error('unexpected dimensions on '+pathFS+': '+png.width+'x'+png.height+' !== '+tileWidth+'x'+tileHeight);
 
-  //png.bitblt(stitched, 0, 0, tileWidth, tileHeight, tileX * tileWidth, tileY * tileHeight);
+    png.bitblt(stitched, 0, 0, tileWidth, tileHeight, tileX * tileWidth, tileY * tileHeight);
 
-  for (var x = 0; x < png.width; ++x) {
-    for (var y = 0; y < png.height; ++y) {
-      var idx = (png.width * y + x) << 2;
+    /*
+    for (var x = 0; x < png.width; ++x) {
+      for (var y = 0; y < png.height; ++y) {
+        var idx = (png.width * y + x) << 2;
 
-      var idxS = (stitched.width * y + x) << 2;
+        var idxS = (stitched.width * y + x) << 2;
 
-      for (var c = 0; c < 4; ++c) {
-        stitched.data[idxS + c] = 100; //png[idx + c];
-        console.log(' data '+x+','+y+','+c+' = '+png.data[idx+c]);
+        for (var c = 0; c < 4; ++c) {
+          stitched.data[idxS + c] = 100; //png[idx + c];
+          console.log(' data '+x+','+y+','+c+' = '+png.data[idx+c]);
+        }
       }
     }
-  }
+    */
+  });
 
 
-  tileX  += 1;
+  tileX += 1;
   //if (x > ) y += 1;
 });
 
