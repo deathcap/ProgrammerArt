35e07a9e6d52e9180bf6b3a4d51948e55b7f62fb
deathcap <deathcap@gmx.co.uk>
2014-01-15 20:27:28 -0800

    Move stitch coordinates to stitchpack_data.js

--- scripts/stitch.js
+++ scripts/stitch.js
@@ -5,105 +5,7 @@ var fs = require('fs');
 
 var tileWidth = 16, tileHeight = 16;
 
-var terrainMatrix = [
-  ['grass_top', 'stone', 'dirt', 'grass_side', 'planks_oak', 'stone_slab_side', 'stone_slab_top', 'brick', 
-  'tnt_side', 'tnt_top', 'tnt_bottom', 'web', 'flower_rose', 'flower_dandelion', 'portal', 'sapling_oak'],
-
-  ['cobblestone', 'bedrock', 'sand', 'gravel', 'log_oak', 'log_oak_top', 'iron_block', 'gold_block', 
-  'diamond_block', 'emerald_block', null, null, 'mushroom_red', 'mushroom_brown', 'sapling_jungle', 'fire_layer_0'],
-
-  ['gold_ore', 'iron_ore', 'coal_ore', 'bookshelf', 'cobblestone_mossy', 'obsidian', 'grass_side', 'tallgrass', 
-  'grass_top', 'beacon', null, 'crafting_table_top', 'furnace_front_off', 'furnace_side', 'dispenser_front_horizontal', 'fire_layer_1'],
-
-  ['sponge', 'glass', 'diamond_ore', 'redstone_ore', 'leaves_oak', 'leaves_oak_opaque', 'stonebrick', 'deadbush', 
-  'fern', null, null, 'crafting_table_side', 'crafting_table_front', 'furnace_front_on', 'furnace_top', 'sapling_spruce'],
-
-  ['wool_colored_white', null/*spawner*/, 'snow', 'ice', 'grass_side_snowed', 'cactus_top', 'cactus_side', 'cactus_bottom',
-  'clay', 'reeds', 'jukebox_side', 'jukebox_top', 'waterlily', 'mycelium_side', 'mycelium_top', 'sapling_birch'],
-
-  ['torch_on', 'door_wood_upper', 'door_iron_upper', 'ladder', 'trapdoor', 'iron_bars', 'farmland_wet', 'farmland_dry', 
-  'wheat_stage_0', 'wheat_stage_1', 'wheat_stage_2', 'wheat_stage_3', 'wheat_stage_4', 'wheat_stage_5', 'wheat_stage_6', 'wheat_stage_7'],
-
-  ['lever', 'door_wood_lower', 'door_iron_lower', 'redstone_torch_on', 'stonebrick_mossy', 'stonebrick_cracked', 'pumpkin_top', 'netherrack',
-  'soul_sand', 'glowstone', 'piston_top_sticky', 'piston_top_normal', 'piston_side', 'piston_bottom', 'piston_inner', 'melon_stem_disconnected'],
-
-  ['rail_normal_turned', 'wool_colored_black', 'wool_colored_gray', 'redstone_torch_off', 'log_spruce', 'log_birch', 'pumpkin_side', 'pumpkin_face_off',
-  'pumpkin_face_on', 'cake_top', 'cake_side', 'cake_inner', 'cake_bottom', 'mushroom_block_skin_red', 'mushroom_block_skin_brown', 'melon_stem_connected'],
-
-  ['rail_normal', 'wool_colored_red', 'wool_colored_pink', 'repeater_off', 'leaves_spruce', 'leaves_spruce_opaque', 'bed_feet_top', 'bed_head_side', 
-  'melon_side', 'melon_top', 'cauldron_top', 'cauldron_inner', 'cake_top', 'mushroom_block_skin_stem', 'mushroom_block_inside', 'vine'],
-
-  ['lapis_block', 'wool_colored_green', 'wool_colored_lime', 'repeater_on', 'glass_pane_top', 'bed_feet_end', 'bed_feet_side', 'bed_head_side',
-  'bed_head_end', 'log_jungle', 'cauldron_side', 'cauldron_bottom', 'brewing_stand_base', 'brewing_stand', 'endframe_top', 'endframe_side'],
-
-  ['lapis_ore', 'wool_colored_brown', 'wool_colored_yellow', 'rail_golden', 'redstone_dust_line', 'redstone_dust_line', 'enchanting_table_top',
-  'dragon_egg', 'cocoa_stage_2', 'cocoa_stage_1', 'cocoa_stage_0', 'emerald_ore', 'trip_wire_source', 'trip_wire', 'endframe_eye', 'end_stone'],
-
-  ['sandstone_top', 'wool_colored_blue', 'wool_colored_light_blue', 'rail_golden_powered', 'redstone_dust_line_overlay', 'redstone_dust_cross_overlay', 'enchanting_table_side', 'enchanting_table_bottom',
-  'command_block', 'itemframe_background', 'flower_pot', null, null, null, null, null],
-
-  ['sandstone_normal', 'wool_colored_purple', 'wool_colored_magenta', 'rail_detector', 'leaves_jungle', 'leaves_jungle_opaque', 'planks_spruce', 'planks_jungle',
-  'carrots_stage_0', 'carrots_stage_1', 'carrots_stage_2', 'carrots_stage_3', 'potatoes_stage_3', 'water_flow', 'water_flow', 'water_flow'],
-
-  ['sandstone_bottom', 'wool_colored_cyan', 'wool_colored_orange', 'redstone_lamp_off', 'redstone_lamp_on', 'stonebrick_carved', 'planks_birch', 'anvil_base', 
-  'anvil_top_damaged_1', null, null, null, null, null, 'water_flow', 'water_flow'],
-
-  ['nether_brick', 'wool_colored_silver', 'nether_wart_stage_0', 'nether_wart_stage_1', 'nether_wart_stage_2', 'sandstone_carved', 'sandstone_smooth', 'anvil_top_damaged_0',
-  'anvil_top_damaged_2', null, null, null, null, 'lava_flow', 'lava_flow', 'lava_flow'],
-
-  ['destroy_stage_0', 'destroy_stage_1', 'destroy_stage_2', 'destroy_stage_3', 'destroy_stage_4', 'destroy_stage_5', 'destroy_stage_6', 'destroy_stage_7',
-  'destroy_stage_8', 'destroy_stage_9', null, null, null, null, 'lava_flow', 'lava_flow']
-];
-
-var itemsMatrix = [
-  ['leather_helmet', 'chainmail_helmet', 'iron_helmet', 'diamond_helmet', 'gold_helmet', 'flint_and_steel', 'flint', 'coal',
-  'string', 'seeds_wheat', 'apple', 'apple_golden', 'egg', 'sugar', 'snowball'],
-  
-  ['leather_chestplate', 'chainmail_chestplate', 'iron_chestplate', 'diamond_chestplate', 'gold_chestplate', 'bow_standby', 'brick', 'iron_ingot',
-  'feather', 'wheat', 'painting', 'reeds', 'bone', 'cake', 'slimeball'],
-
-  ['leather_leggings', 'chainmail_leggings', 'iron_leggings', 'diamond_leggings', 'gold_leggings', 'arrow', 'quiver', 'gold_ingot',
-  'gunpowder', 'bread', 'sign', 'door_wood', 'door_iron', 'bed', 'fireball'],
-
-  ['leather_boots', 'chainmail_boots', 'iron_boots', 'diamond_boots', 'gold_boots', 'stick', 'compass', 'diamond',
-  'redstone_dust', 'clay_ball', 'paper', 'book_normal', 'map_filled', 'seeds_pumpkin', 'seeds_melon'],
-
-  ['wood_sword', 'stone_sword', 'iron_sword', 'diamond_sword', 'gold_sword', 'fishing_rod_cast', 'clock', 'bowl', 
-  'mushroom_stew', 'glowstone_dust', 'bucket_empty', 'bucket_water', 'bucket_lava', 'bucket_milk', 'dye_powder_black', 'dye_powder_gray'],
-
-  ['wood_shovel', 'stone_shovel', 'iron_shovel', 'diamond_shovel', 'gold_shovel', 'fishing_rod_uncast', 'repeater', 'porkchop_raw',
-  'porkchop_cooked', 'fish_cod_raw',  'fish_cod_cooked', 'rotten_flesh', 'cookie', 'shears', 'dye_powder_red', 'dye_powder_pink'],
-
-  ['wood_pickaxe', 'stone_pickaxe', 'iron_pickaxe', 'diamond_pickaxe', 'gold_pickaxe', 'bow_pulling_0', 'carrot_on_a_stick', 'leather',
-  'saddle', 'beef_raw', 'beef_cooked', 'ender_pearl', 'blaze_rod', 'melon', 'dye_powder_green', 'dye_powder_lime'],
-
-  ['wood_axe', 'stone_axe', 'iron_axe', 'diamond_axe', 'gold_axe', 'bow_pulling_1', 'potato_baked', 
-  'carrot', 'chicken_raw', 'chicken_cooked', 'ghast_tear', 'gold_nugget', 'nether_wart', 'dye_powder_brown', 'dye_powder_yellow'],
-
-  ['wood_hoe', 'stone_hoe', 'iron_hoe', 'diamond_hoe', 'gold_hoe', 'bow_pulling_2', 'potato_poisonous', 'minecart_normal',
-  'boat', 'melon_speckled', 'spider_eye_fermented', 'spider_eye', 'potion_bottle_empty', 'potion_overlay', 'dye_powder_blue', 'dye_powder_cyan'],
-
-  ['leather_helmet_overlay', null, null, null, null, null, 'carrot_golden', 'minecart_chest',
-  'pumpkin_pie', 'spawn_egg', 'potion_bottle_splash', 'ender_eye', 'cauldron', 'blaze_powder', 'dye_powder_light_blue', 'dye_powder_magenta'],
-
-  ['leather_chestplate_overlay', null, null, null, null, null, 'minecart_furnace', null, 'spawn_egg_overlay', 
-  'ruby', 'experience_bottle', 'magma_cream', 'dye_powder_cyan', 'dye_powder_orange'],
-  
-  ['leather_leggings_overlay', null, null, null, null, null, null,
-  null, 'nether_star', 'emerald', 'book_writable', 'book_written', 'flower_pot', 'dye_powder_silver', 'dye_powder_white'],
-
-  ['leather_boots_overlay', null, null, null, null, null, null,
-  'fireworks', 'fireworks_charge', 'fireworks_charge_overlay', null, 'map_empty', 'item_frame', 'book_enchanted'],
-
-  [null, null, null, null, null, null, null, null,
-  null, null, null, null, null, null, null, null],
-
-  ['skull_skeleton', 'skull_wither', 'skull_zombie', 'skull_steve', 'skull_creeper', null, null,
-  null, null, null, null, null, null, null, null],
-
-  ['record_13', 'record_cat', 'record_blocks', 'record_chirp', 'record_far', 'record_mall', 'record_mellohi', 'record_stal',
-  'record_strad', 'record_ward', 'record_11', 'record_wait', 'record_wait', 'record_wait', 'record_wait', 'record_wait']
-];
+var matrices = require('./stitchpack_data.js');
 
 var coverArea = function(h, w, cb) {
   for (var y = 0; y < h; y += 1) {
@@ -154,6 +56,6 @@ var stitch = function(outFile, pathPrefix, matrix) {
   });
 };
 
-stitch('terrain.png', '../textures/blocks/', terrainMatrix);
-stitch('items.png', '../textures/items/', itemsMatrix);
+stitch('terrain.png', '../textures/blocks/', matrices.terrain);
+stitch('items.png', '../textures/items/', matrices.items);
 
