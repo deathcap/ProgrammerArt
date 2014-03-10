ec1eb8f241b47bc66e4feb46716477dfedd37739
deathcap <deathcap@gmx.co.uk>
2014-03-09 17:15:43 -0700

    Start refactoring mod texture name expansion programmatically

--- scripts/modpack_data.js
+++ scripts/modpack_data.js
@@ -12,3 +12,43 @@ module.exports = {
   'tin_block': ['thermalexpansion/blocks/storage/BlockTin', 'ic2/textures/blocks/blockMetalTin', 'ic2/textures/blocks/blockMetalTin', 'metallurgy/textures/blocks/Base/TinBlock'],
 };
 
+var templates = {
+  ingot: ['thermalexpansion/items/material/Ingot_<UCFIRST>', 'ic2/textures/items/resources/itemIngot<UCFIRST>', 'forestry/textures/items/ingot<UCFIRST>'],
+  dust: ['thermalexpansion/items/material/Dust<UCFIRST>', 'ic2/textures/items/resources/itemDust<UCFIRST>'],
+  ore: ['thermalexpansion/blocks/ores/Ore_<UCFIRST>', 'ic2/textures/blocks/blockOre<UCFIRST>', 'tinker/textures/blocks/ore_<LOWER>', 'forestry/textures/blocks/ores/<LOWER>', 'metallurgy/textures/blocks/Base/<UCFIRST>Ore'],
+  block: ['thermalexpansion/blocks/storage/Block_<UCFIRST>', 'ic2/textures/blocks/blockMetal<UCFIRST>', 'metallurgy/textures/blocks/Base/<UCFIRST>Block'],
+};
+
+var bases = {
+  copper: ['ingot', 'dust', 'ore', 'block'],
+  tin: ['ingot', 'dust', 'ore', 'block'],
+}; 
+
+var ucfirst = function(s) {
+    return s.substr(0, 1).toUpperCase() + s.substring(1);
+};
+/*
+var lookupTemplates = function(base, form) {
+  for (var key in templates) {
+    var keySubst = substituteVariables(key, base);
+    if (keySubst ===  // TODO
+  }
+}*/
+
+var substituteVariables = function(s, value) {
+  return s.replace('<LOWER>', value).replace('<UCFIRST>', ucfirst(value));
+};
+
+for (var base in bases) {
+  bases[base].forEach(function(form) {
+    var templateArray = templates[form];
+    if (!templateArray) throw new Error('no template found for form '+form+' of '+base);
+
+    templateArray.forEach(function(template) {
+
+      var name = substituteVariables(template, base);
+
+      console.log(name);
+    });
+  });
+}
