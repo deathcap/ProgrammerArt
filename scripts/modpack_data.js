d0b412b11bba683be27c48d24c796d819ececbd3
deathcap <deathcap@gmx.co.uk>
2014-03-09 19:58:41 -0700

    Add tin/copper dusts and blocks
    Ref GH-13

--- scripts/modpack_data.js
+++ scripts/modpack_data.js
@@ -1,29 +1,14 @@
 'use strict';
 
-/*
-module.exports = {
-  'copper_ingot': ['thermalexpansion/items/material/IngotCopper', 'ic2/textures/items/resources/itemIngotCopper', 'forestry/textures/items/ingotCopper'],
-  'copper_dust': ['thermalexpansion/items/material/DustCopper', 'ic2/textures/items/resources/itemDustCopper'],
-  'copper_ore': ['thermalexpansion/blocks/ores/OreCopper', 'ic2/textures/blocks/blockOreCopper', 'tinker/textures/blocks/ore_copper', 'forestry/textures/blocks/ores/copper', 'metallurgy/textures/blocks/Base/CopperOre'],
-  'copper_block': ['thermalexpansion/blocks/storage/BlockCopper', 'ic2/textures/blocks/blockMetalCopper', 'metallurgy/textures/blocks/Base/CopperBlock'],
-
-  'tin_ingot': ['thermalexpansion/items/material/IngotTin', 'ic2/textures/items/resources/itemIngotTin', 'forestry/textures/items/ingotTin'],
-  'tin_dust': ['thermalexpansion/items/material/DustTin', 'ic2/textures/items/resources/itemDustTin'],
-  'tin_ore': ['thermalexpansion/blocks/ores/OreTin', 'ic2/textures/blocks/blockOreTin', 'tinker/textures/blocks/ore_tin', 'forestry/textures/blocks/ores/tin', 'metallurgy/textures/blocks/Base/TinOre'],
-  'tin_block': ['thermalexpansion/blocks/storage/BlockTin', 'ic2/textures/blocks/blockMetalTin', 'metallurgy/textures/blocks/Base/TinBlock'],
-};
-*/
-
 var templates = {
   ingot: ['thermalexpansion/items/material/Ingot<UCFIRST>', 'ic2/textures/items/resources/itemIngot<UCFIRST>', 'tinker/textures/items/materials/material_<LOWER>ingot', 'forestry/textures/items/ingot<UCFIRST>'],
-  //TODO dust: ['thermalexpansion/items/material/Dust<UCFIRST>', 'ic2/textures/items/resources/itemDust<UCFIRST>'],
+  dust: ['thermalexpansion/items/material/Dust<UCFIRST>', 'ic2/textures/items/resources/itemDust<UCFIRST>'],
   ore: ['thermalexpansion/blocks/ores/Ore<UCFIRST>', 'ic2/textures/blocks/blockOre<UCFIRST>', 'tinker/textures/blocks/ore_<LOWER>', 'forestry/textures/blocks/ores/<LOWER>', 'metallurgy/textures/blocks/Base/<UCFIRST>Ore'],
-  //TODO block: ['thermalexpansion/blocks/storage/Block<UCFIRST>', 'ic2/textures/blocks/blockMetal<UCFIRST>', 'metallurgy/textures/blocks/Base/<UCFIRST>Block'],
+  block: ['thermalexpansion/blocks/storage/Block<UCFIRST>', 'ic2/textures/blocks/blockMetal<UCFIRST>', 'tinker/textures/blocks/compressed_<LOWER>', 'metallurgy/textures/blocks/Base/<UCFIRST>Block'],
 };
 
 
-//var ALL_FORMS = ['ingot', 'dust', 'ore', 'block']; // TODO
-var ALL_FORMS = ['ingot', 'ore'];
+var ALL_FORMS = ['ingot', 'dust', 'ore', 'block'];
 
 var bases = {
   copper: ALL_FORMS,
@@ -59,13 +44,6 @@ for (var base in bases) {
     result[category][ourName] = theirNames;
   });
 }
-
-/*
-console.log(JSON.stringify(module.exports,null,'  '));
-console.log(JSON.stringify(result,null,'  '));
-
-console.log(JSON.stringify(module.exports) == JSON.stringify(result));
-*/
 
 module.exports = result;
 console.log(result);
