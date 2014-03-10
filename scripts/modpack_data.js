58d5bc34885b0583099b6fb101100afa778395de
deathcap <deathcap@gmx.co.uk>
2014-03-09 18:18:56 -0700

    modpack_data: categorize as blocks/items

--- scripts/modpack_data.js
+++ scripts/modpack_data.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 module.exports = {
   'copper_ingot': ['thermalexpansion/items/material/IngotCopper', 'ic2/textures/items/resources/itemIngotCopper', 'forestry/textures/items/ingotCopper'],
   'copper_dust': ['thermalexpansion/items/material/DustCopper', 'ic2/textures/items/resources/itemDustCopper'],
@@ -11,35 +12,33 @@ module.exports = {
   'tin_ore': ['thermalexpansion/blocks/ores/OreTin', 'ic2/textures/blocks/blockOreTin', 'tinker/textures/blocks/ore_tin', 'forestry/textures/blocks/ores/tin', 'metallurgy/textures/blocks/Base/TinOre'],
   'tin_block': ['thermalexpansion/blocks/storage/BlockTin', 'ic2/textures/blocks/blockMetalTin', 'metallurgy/textures/blocks/Base/TinBlock'],
 };
+*/
 
 var templates = {
   ingot: ['thermalexpansion/items/material/Ingot<UCFIRST>', 'ic2/textures/items/resources/itemIngot<UCFIRST>', 'forestry/textures/items/ingot<UCFIRST>'],
-  dust: ['thermalexpansion/items/material/Dust<UCFIRST>', 'ic2/textures/items/resources/itemDust<UCFIRST>'],
+  //TODO dust: ['thermalexpansion/items/material/Dust<UCFIRST>', 'ic2/textures/items/resources/itemDust<UCFIRST>'],
   ore: ['thermalexpansion/blocks/ores/Ore<UCFIRST>', 'ic2/textures/blocks/blockOre<UCFIRST>', 'tinker/textures/blocks/ore_<LOWER>', 'forestry/textures/blocks/ores/<LOWER>', 'metallurgy/textures/blocks/Base/<UCFIRST>Ore'],
-  block: ['thermalexpansion/blocks/storage/Block<UCFIRST>', 'ic2/textures/blocks/blockMetal<UCFIRST>', 'metallurgy/textures/blocks/Base/<UCFIRST>Block'],
+  //TODO block: ['thermalexpansion/blocks/storage/Block<UCFIRST>', 'ic2/textures/blocks/blockMetal<UCFIRST>', 'metallurgy/textures/blocks/Base/<UCFIRST>Block'],
 };
 
+
+//var ALL_FORMS = ['ingot', 'dust', 'ore', 'block']; // TODO
+var ALL_FORMS = ['ingot', 'ore'];
+
 var bases = {
-  copper: ['ingot', 'dust', 'ore', 'block'],
-  tin: ['ingot', 'dust', 'ore', 'block'],
+  copper: ALL_FORMS,
+  tin: ALL_FORMS,
 }; 
 
 var ucfirst = function(s) {
     return s.substr(0, 1).toUpperCase() + s.substring(1);
 };
-/*
-var lookupTemplates = function(base, form) {
-  for (var key in templates) {
-    var keySubst = substituteVariables(key, base);
-    if (keySubst ===  // TODO
-  }
-}*/
 
 var substituteVariables = function(s, value) {
   return s.replace('<LOWER>', value).replace('<UCFIRST>', ucfirst(value));
 };
 
-var result = {};
+var result = {blocks: {}, items: {}};
 for (var base in bases) {
   bases[base].forEach(function(form) {
     var ourName = base + '_' + form;
@@ -48,16 +47,25 @@ for (var base in bases) {
     var templateArray = templates[form];
     if (!templateArray) throw new Error('no template found for form '+form+' of '+base);
 
+    var isBlock = false;
+
     theirNames = templateArray.map(function(template) {
+      if (template.indexOf('/blocks/') !== -1) isBlock = true;
       return substituteVariables(template, base);
     });
 
-    result[ourName] = theirNames;
+    var category = isBlock ? 'blocks' : 'items';
+
+    result[category][ourName] = theirNames;
   });
 }
 
+/*
 console.log(JSON.stringify(module.exports,null,'  '));
 console.log(JSON.stringify(result,null,'  '));
 
 console.log(JSON.stringify(module.exports) == JSON.stringify(result));
+*/
 
+module.exports = result;
+console.log(result);
