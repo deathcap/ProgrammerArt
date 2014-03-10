d76028c8ce7ec300d459b83d9c82fad553f78f6c
deathcap <deathcap@gmx.co.uk>
2014-03-09 17:05:29 -0700

    Add modpack data for copper and tin

---
+++ scripts/modpack_data.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  'copper_ingot': ['thermalexpansion/items/material/Ingot_Copper', 'ic2/textures/items/resources/itemIngotCopper', 'forestry/textures/items/ingotCopper'],
+  'copper_dust': ['thermalexpansion/items/material/DustCopper', 'ic2/textures/items/resources/itemDustCopper'],
+  'copper_ore': ['thermalexpansion/blocks/ores/Ore_Copper', 'ic2/textures/blocks/blockOreCopper', 'tinker/textures/blocks/ore_copper', 'forestry/textures/blocks/ores/copper', 'metallurgy/textures/blocks/Base/CopperOre'],
+  'copper_block': ['thermalexpansion/blocks/storage/Block_Copper', 'ic2/textures/blocks/blockMetalCopper', 'metallurgy/textures/blocks/Base/CopperBlock'],
+
+  'tin_ingot': ['thermalexpansion/items/material/Ingot_Tin', 'ic2/textures/items/resources/itemIngotTin', 'assets/forestry/textures/items/ingotTin'],
+  'tin_dust': ['thermalexpansion/items/material/DustTin', 'ic2/textures/items/resources/itemDustTin', 'tinker/textures/blocks/ore_tin', 'forestry/textures/blocks/ores/tin', 'metallurgy/textures/blocks/Base/TinOre'],
+  'tin_ore': ['thermalexpansion/blocks/ores/OreTin', 'ic2/textures/blocks/blockOreTin'],
+  'tin_block': ['thermalexpansion/blocks/storage/BlockTin', 'ic2/textures/blocks/blockMetalTin', 'ic2/textures/blocks/blockMetalTin', 'metallurgy/textures/blocks/Base/TinBlock'],
+};
+
